b2f05d6eb406e6fa86a1f182c6ce4aac4d7ec196
Neha Aggarwal <neha68977@gmail.com>
2021-08-03 14:26:27 +0530

    used async and await

--- script.js
+++ script.js
@@ -3,19 +3,40 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 // GET https://icanhazdadjoke.com/
 
-const generateJokes = () => {
-  const setHeader = {
-    headers: {
-      Accept: "application/json",
-    },
-  };
+// ********* using PROMISES ********
 
-  fetch("https://icanhazdadjoke.com", setHeader)
-    .then((res) => res.json())
-    .then((data) => (jokes.innerHTML = data.joke))
-    .catch((err) => {
-      console.log(err);
-    });
+// const generateJokes = () => {
+
+//   const setHeader = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+
+//   fetch("https://icanhazdadjoke.com", setHeader)
+//     .then((res) => res.json())
+//     .then((data) => (jokes.innerHTML = data.joke))
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
+
+// *********** Using async await *************
+
+const generateJokes = async () => {
+  try {
+    const setHeader = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+
+    const response = await fetch("https://icanhazdadjoke.com/", setHeader);
+    const data = await response.json();
+    jokes.innerHTML = data.joke;
+  } catch (err) {
+    console.log(`the error is ${err}`);
+  }
 };
 
 jokeBtn.addEventListener("click", generateJokes);
